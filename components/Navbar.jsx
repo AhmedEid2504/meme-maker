0ffcbfa48e3b00601b5f35378335b6843c69e9b8
Ahmed Eid <ahmedeid2504@gmail.com>
2024-03-07 21:13:15 +0200

    adds to component Navbar and style

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,16 @@
 
-const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Navbar =() => {
+    return (
+        <nav className="navbar">
+            <img 
+                className="logo"
+                src="../public/images/troll-face.png"
+                alt="meme maker logo(troll face)"
+            />
+            <h1 className="logo-text">Meme Maker</h1>
+            
+        </nav>
+    )
 }
 
-export default Navbar
+export default Navbar;
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from '../components/Navbar';
-import './App.css'
+import './index.css'
 
 function App() {
   

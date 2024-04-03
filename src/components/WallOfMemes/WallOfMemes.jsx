13a97230e020693069f65a28429554982adc34d9
Ahmed Eid <ahmedeid2504@gmail.com>
2024-04-03 05:12:21 +0200

    adds delete functionality to wall of memes users can delete only memes that they posted button wont show for other users or guests

--- src/components/WallOfMemes/WallOfMemes.jsx
+++ src/components/WallOfMemes/WallOfMemes.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
 import { storage } from '../../firebase/firebase';
-import { ref, listAll, getDownloadURL } from 'firebase/storage';
+import { ref, listAll, getDownloadURL, deleteObject } from 'firebase/storage';
 import './wallofmemes.css';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useAuth } from '../../contexts/authContext';
 const WallOfMemes = () => {
-    const downloadNotify = () => toast("Meme Downloaded To Your Device");
+    const downloadNotify = () => toast("Meme Downloaded To Your Device", {type: "success"});
+    const deleteNotify = () => toast("Meme Deleted Successfully", {type: "info"});
     const [imageUrls, setImageUrls] = useState([]);
+    const {currentUser} = useAuth()
 
     useEffect(() => {
         const fetchImagesFromAllUsers = async () => {
@@ -17,24 +20,29 @@ const WallOfMemes = () => {
 
                 for (const userFolder of userFolders.prefixes) {
                     const userImages = await listAll(userFolder);
-
+                
                     for (const imageRef of userImages.items) {
                         const downloadURL = await getDownloadURL(imageRef);
                         const fileName = imageRef.name;
-
+                
+                        // Extract user ID from the image name
+                        const userIdStartIndex = fileName.indexOf('Meme-Maker_') + 'Meme-Maker_'.length;
+                        const userIdEndIndex = fileName.indexOf('_At_');
+                        const userId = fileName.substring(userIdStartIndex, userIdEndIndex);
+                
                         // Extract timestamp from the filename
-                        const timestampIndex = fileName.lastIndexOf('_At_'); // Finding the last occurrence of '_At_' in the filename
-                        const timestampString = fileName.substring(timestampIndex + 4, fileName.lastIndexOf('.png')); // Extracting the timestamp substring
+                        const timestampIndex = fileName.lastIndexOf('_At_');
+                        const timestampString = fileName.substring(timestampIndex + 4, fileName.lastIndexOf('.png'));
                         const createTime = new Date(timestampString);
-
-                        urls.push({ downloadURL, createTime });
+                
+                        urls.push({ downloadURL, createTime, userId });
                     }
                 }
 
                 // Sort URLs based on createTime in descending order
                 urls.sort((a, b) => b.createTime - a.createTime);
 
-                setImageUrls(urls.map(item => item.downloadURL));
+                setImageUrls(urls);
             } catch (error) {
                 console.error('Error fetching images:', error);
             }
@@ -56,6 +64,7 @@ const WallOfMemes = () => {
                     link.setAttribute("download", "image.png"); // Change the filename as needed
                     document.body.appendChild(link);
                     link.click();
+                    console.log(imageUrl.userId);
                 });
             })
             .catch(err => {
@@ -64,7 +73,17 @@ const WallOfMemes = () => {
         downloadNotify();
     };
 
-    
+    const handleDelete = async (imageUrl) => {
+        const imageRef = ref(storage, imageUrl);
+        try {
+            await deleteObject(imageRef);
+            const updatedUrls = imageUrls.filter((image) => image.downloadURL !== imageUrl);
+            setImageUrls(updatedUrls);
+            deleteNotify();
+        } catch (error) {
+            console.error('Error deleting image:', error);
+        }
+    };
 
     
 
@@ -75,12 +94,20 @@ const WallOfMemes = () => {
                 <div className="meme-cards-container">
                     {imageUrls.map((imageUrl, index) => (
                         <div key={index} className='meme-card'>
-                            <img src={imageUrl} alt={`Meme ${index}`} />
+                            <img src={imageUrl.downloadURL} alt={`Meme ${index}`} />
                             <div className="card-buttons">
                                 <div className="card-button">
-                                    <button id='download' className="card-button-download" onClick={() => handleDownload(imageUrl)}><img src="images/download.png" alt="download icon" /></button>
+                                    <button id='download' className="card-button-download" onClick={() => handleDownload(imageUrl.downloadURL)}><img src="images/download.png" alt="download icon" /></button>
                                     <label htmlFor='download'>Download</label>
                                 </div>
+                                { currentUser && currentUser.uid === imageUrl.userId && (
+                                    <div className="card-button">
+                                        <button className="card-button-delete" onClick={() => handleDelete(imageUrl.downloadURL)}>
+                                            <img src="images/delete.png" alt="delete icon" />
+                                        </button>
+                                        <label>Delete</label>
+                                    </div>
+                                )}
                             </div>
                         </div>
                     ))}

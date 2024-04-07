a743bc6cf49175cabe8215d323fe39bc31a20542
Ahmed Eid <ahmedeid2504@gmail.com>
2024-04-07 06:39:24 +0200

    translates signup into tailwind

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -42,75 +42,81 @@ const Signup = () => {
     };
 
     return (
-        <div className="main-container">
-            <div className="main">
-                <div className="signup-container">
-                    <h2>Sign Up</h2>
-                    <form className='signup-form' onSubmit={handleSubmit}>
-                        <div className="form-group">
-                            <label htmlFor="name">Name</label>
-                            <input
-                                className='signup-input'
-                                type="text"
-                                id="name"
-                                name="name"
-                                value={formData.name}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input
-                                className='signup-input'
-                                type="email"
-                                id="email"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="phoneNumber">Phone Number</label>
-                            <input
-                                className='signup-input'
-                                type="tel"
-                                id="phoneNumber"
-                                name="phoneNumber"
-                                value={formData.phoneNumber}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <input
-                                className='signup-input'
-                                type="password"
-                                id="password"
-                                name="password"
-                                value={formData.password}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="confirmPassword">Confirm Password</label>
-                            <input
-                                className='signup-input'
-                                type="password"
-                                id="confirmPassword"
-                                name="confirmPassword"
-                                value={formData.confirmPassword}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                        <button className='signup-button' type="submit">Sign Up</button>
-                    </form>
+        <div className="flex flex-col justify-center items-center
+                        max-w-50 border-4 border-third p-5 rounded-md"
+        >
+            <h2>Sign Up</h2>
+            <form className='flex flex-col gap-5 p-2' onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input
+                        className='w-[100%] h-8 bg-third rounded-sm p-2 border-none'
+                        type="text"
+                        id="name"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                    />
                 </div>
-            </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        className='w-[100%] h-8 bg-third rounded-sm p-2 border-none'
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="phoneNumber">Phone Number</label>
+                    <input
+                        className='w-[100%] h-8 bg-third rounded-sm p-2 border-none'
+                        type="tel"
+                        id="phoneNumber"
+                        name="phoneNumber"
+                        value={formData.phoneNumber}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        className='w-[100%] h-8 bg-third rounded-sm p-2 border-none'
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <input
+                        className='w-[100%] h-8 bg-third rounded-sm p-2 border-none'
+                        type="password"
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        value={formData.confirmPassword}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <button 
+                    className="bg-primary shadow-md shadow-fourth focus:shadow-inner
+                                    focus:shadow-primary p-2 rounded-md hover:bg-secondary
+                                    hover:text-third border-2 border-primary transition-all 
+                                    duration-200 ease-in
+                            " 
+                    type="submit">
+                    Sign Up
+                </button>
+            </form>
         </div>
     );
 };
